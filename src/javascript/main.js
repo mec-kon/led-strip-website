29cdc0d0f3dd095ca811987cc944f567d7190ec5
mec-kon <mec-kon@web.de>
2018-10-14 14:24:38 +0200

    added config.json file

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,32 +1,60 @@
-var xmlhttp = new XMLHttpRequest();
-var maxNumberOfColors = 6;
+var config;
 
+loadJSON(function(response) {
+    // Parse JSON string into object
+    config = JSON.parse(response);
+});
+
+let maxNumberOfColors = 6;
 
 function getColor(colorString) {
     colorString = colorString.replace("rgb(", "");
     colorString = colorString.replace(")", "");
 
-    var colors = colorString.split(",");
+    let colors = colorString.split(",");
 
     return colors;
 }
 
 function postToJson(data) {
+    var request = new XMLHttpRequest();
+    let url = "http://" + config["devices"][0]["ipAddress"] + ":" + config["devices"][0]["port"] +"/colors.json";
+    let json = JSON.stringify(data);
 
-    var url = "http://localhost:9999/colors.json";
-
-    var json = JSON.stringify(data);
-
-    xmlhttp.open("POST", url, true);
-    xmlhttp.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    xmlhttp.onload = function () {
-        var data = JSON.parse(xmlhttp.responseText);
-        if (xmlhttp.readyState == 4 && xmlhttp.status == "201") {
+    request.open("POST", url, true);
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.onload = function () {
+        var data = JSON.parse(request.responseText);
+        if (request.readyState == 4 && request.status == "201") {
             console.table(data);
         } else {
             console.error(data);
         }
     }
-    xmlhttp.send(json);
-    console.log(json);
+    request.send(json);
+    console.log(url);
+}
+
+
+function loadJSON(callback) {
+
+    var request = new XMLHttpRequest();
+    request.open('GET', 'config.json', true);
+    request.onload = function () {
+        if (request.readyState == 4 && request.status == "200") {
+            callback(request.responseText);
+        }
+    };
+    request.send(null);
+}
+
+function loadSiteContent(url) {
+    var request = new XMLHttpRequest();
+    request.open("GET", url, true);
+    request.onload = function() {
+        if (request.readyState == 4 && request.status == "200") {
+            document.getElementById("banner").innerHTML = request.responseText;
+        }
+    };
+    request.send();
 }
