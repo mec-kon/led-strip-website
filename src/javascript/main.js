7bbe54c9dabac0c96ee843636de1a1e308f91e79
mec-kon <mec-kon@web.de>
2018-10-31 19:25:16 +0100

    copied methods turnON() and turnOFF() to main.js so that they can be called by all methods

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -76,3 +76,45 @@ function loadSiteContent(url) {
         current_mode = url
     }
 }
+
+function turnOFF() {
+    document.getElementById("onOffOneColor").innerHTML = "activate";
+    stripLights = false;
+
+    let data = {};
+    let color = {};
+    let color_array = [];
+
+    color.color_red = 0;
+    color.color_green = 0;
+    color.color_blue = 0;
+    color_array[0] = color;
+
+    data.color_array = color_array;
+    data.time = 0;
+    data.mode = "oneColor";
+    data.number_of_colors = 1;
+
+    postToJson(data, "colors.json");
+}
+
+function turnON() {
+    document.getElementById("onOffOneColor").innerHTML = "deactivate";
+    stripLights = true;
+
+    let data = {};
+    let color = {};
+    let color_array = [];
+
+    color.color_red = 255;
+    color.color_green = 255;
+    color.color_blue = 255;
+    color_array[0] = color;
+
+    data.color_array = color_array;
+    data.time = 0;
+    data.mode = "oneColor";
+    data.number_of_colors = 1;
+
+    postToJson(data, "colors.json");
+}
--- src/javascript/one_color.js
+++ src/javascript/one_color.js
@@ -16,49 +16,6 @@ function turnOnOff() {
 }
 
 
-function turnOFF() {
-    document.getElementById("onOffOneColor").innerHTML = "activate";
-    stripLights = false;
-
-    let data = {};
-    let color = {};
-    let color_array = [];
-
-    color.color_red = 0;
-    color.color_green = 0;
-    color.color_blue = 0;
-    color_array[0] = color;
-
-    data.color_array = color_array;
-    data.time = 0;
-    data.mode = "oneColor";
-    data.number_of_colors = 1;
-
-    postToJson(data, "colors.json");
-}
-
-function turnON() {
-    document.getElementById("onOffOneColor").innerHTML = "deactivate";
-    stripLights = true;
-
-    let data = {};
-    let color = {};
-    let color_array = [];
-
-    color.color_red = 255;
-    color.color_green = 255;
-    color.color_blue = 255;
-    color_array[0] = color;
-
-    data.color_array = color_array;
-    data.time = 0;
-    data.mode = "oneColor";
-    data.number_of_colors = 1;
-
-    postToJson(data, "colors.json");
-}
-
-
 function setColor() {
     document.getElementById("onOffOneColor").innerHTML = "deactivate";
     stripLights = true;
